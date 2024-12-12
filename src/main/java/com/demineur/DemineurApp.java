22ce879ebc7ae0824fe6de6dfb0feb907eb828cc
Gwenaelle <75257467+gwendvr@users.noreply.github.com>
2024-12-12 14:59:47 +0100

    change app for fullscreen

--- src/main/java/com/demineur/DemineurApp.java
+++ src/main/java/com/demineur/DemineurApp.java
@@ -3,6 +3,8 @@ package com.demineur;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class DemineurApp extends Application {
@@ -11,22 +13,30 @@ public class DemineurApp extends Application {
     public void start(Stage primaryStage) throws Exception {
         // Charger le menu
         FXMLLoader menuLoader = new FXMLLoader(getClass().getResource("menu.fxml"));
-        Scene menuScene = new Scene(menuLoader.load());
+        VBox menuRoot = menuLoader.load();
 
         // Charger la grille de jeu
         FXMLLoader gameLoader = new FXMLLoader(getClass().getResource("main.fxml"));
-        Scene gameScene = new Scene(gameLoader.load());
+        GridPane gameRoot = gameLoader.load();
         DemineurController gameController = gameLoader.getController();
 
-        // Configurer le contrôleur du menu pour basculer entre les scènes
-        DemineurMenuController menuController = menuLoader.getController();
-        menuController.setGameScene(gameScene, primaryStage, gameController);
+        // Créer une seule scène avec le menu comme contenu initial
+        Scene mainScene = new Scene(menuRoot, 800, 600);
 
-        // Démarrer avec la scène du menu
-        primaryStage.setScene(menuScene);
+        // Configurer le contrôleur de menu pour basculer les contenus
+        DemineurMenuController menuController = menuLoader.getController();
+        menuController.setGameRoot(gameRoot, mainScene, primaryStage, gameController);
+
+        primaryStage.setScene(mainScene);
         primaryStage.setTitle("Démineur");
+
+        // Activer le plein écran
+        primaryStage.setFullScreen(true);
+
         primaryStage.show();
     }
+
+
 
     public static void main(String[] args) {
         launch(args);
